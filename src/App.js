3680e49edb808d6a3dad8792d91071b987597a47
Nicholas Myers <nic338@gmail.com>
2024-03-01 14:57:04 -0500

    Added background image and some styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Typography className='header' variant='h2' align='left' marginTop={10} marginLeft={15}>
+      Hi, I'm Nic. Welcome to my site.
+    </Typography>
+    <Container className="aboutMe" maxWidth="sm" sx={{ marginTop: 6}}>
+    <Typography variant='body1'>
+      I'm a junior full stack developer new to the world of tech. I've spent the last decade or so in the retail business developing skills in team building and customer service. In 2023 I took an exciting leap into a new career path with the help of a software development program available to people here in my home state of West Virginia called NewForce. This site is dedicated to showing you some of the work I've accomplished, as well as a way for anyone to contact me with any questions.
+    </Typography>
+    </Container>
+    </>
   );
 }
 
